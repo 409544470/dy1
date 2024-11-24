603b17fd6fd1ba7370a3b07882e78c3112c8dad3
409544470 <58407544+409544470@users.noreply.github.com>
2024-11-24 15:46:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,9 @@
 
 // 部署完成后在网址后面加上这个，获取自建节点和机场聚合节点，/?token=auto或/auto或
 
-let mytoken = 'auto'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
-let BotToken =''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
-let ChatID =''; //可以为空，或者@userinfobot中获取，/start
+let mytoken = 'xuyuwuhu'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
+let BotToken ='7121921499:AAGsNxiF9fv-H86WHS0kBEgbAUM2smXXm9g'; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
+let ChatID ='5635579768'; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
 let FileName = 'CF-Workers-SUB';
 let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
@@ -12,8 +12,7 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
-https://sub.xf.free.hr/auto
+hysteria2://0b88573f-9453-4586-b5ec-32a5e37ba11b@vv6.wjjs.fun:8668/?sni=vv6.wjj.cool&insecure=0#hy2-home
 `
 
 let urls = [];
